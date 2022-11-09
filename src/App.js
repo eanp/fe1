5cbb5195a35bf57dadc61e0b3b99fd8ed5a82ee1
eanp <awifamz@gmail.com>
2022-11-09 11:25:03 +0700

    profile example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import About from './pages/About'
+import Home from './pages/Home'
+import Product from './pages/Product'
+import Profile from './pages/Profile'
+import {BrowserRouter,Route,Link,Routes,Navigate} from 'react-router-dom'
 
 function App() {
+  const [title,setTitle] = useState("E coomerce")
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {title}
       </header>
+      <BrowserRouter>
+        <nav>
+            <Link to="/">Home</Link>
+          
+            <Link to="/product">Product</Link>
+          
+            <Link to="/about">About</Link>
+          
+            <Link to="/profile">Profile</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Navigate to='/home' />} replace="true" />
+          <Route path='/home' element={<Home />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
