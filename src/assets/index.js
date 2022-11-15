e409eecac5a6fc6caf74e1ec3a119dd081b2dd13
eanp <awifamz@gmail.com>
2022-11-15 16:03:38 +0700

    sidebar

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import edit from './edit.png'
+import cubeLogo from './cube-logo.png'
+import homeLogo from './home-logo.png'
+import cartLogo from './cart-logo.png'
+
+const Assets  = {
+    edit,cubeLogo,homeLogo,cartLogo
+}
+
+export default Assets
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 import styles from'./Profile.module.css'
 import DatePicker from "react-datepicker";
+import Assets from './../../assets'
+import Accordion from 'react-bootstrap/Accordion';
 
 import "react-datepicker/dist/react-datepicker.css";
 export default function Profile() {
@@ -21,7 +23,61 @@ export default function Profile() {
         })
     },[])
   return (
-    <div className='bg-light w-100 h-100'>
+    <div className='bg-light w-100 h-100 d-flex flex-row'>
+        <div className="col-3 d-flex flex-column pt-5 " style={{height:"90vh",backgroundColor:"white"}}>
+
+            <div className="justify-content-center d-flex flex-row ">
+                
+                <div className="d-flex flex-row h-25 justify-content-center ">
+                <img src="https://res.cloudinary.com/demo/image/facebook/65646572251.jpg" alt="" className='img rounded-pill' style={{height:"120px"}} />
+                <div className="d-flex flex-column h-50 mt-3 ms-2">
+                <h6>Johanes Michael</h6>
+                <p>
+                    <img src={Assets.edit} alt="" />
+                    Ubah profile</p>
+                </div>
+            </div>
+            </div>
+
+                <div className="w-50 justify-content-center d-flex flex-column mt-5 ms-5">
+                    <p className='fw-bold'>
+                    <img src={Assets.homeLogo} alt="" className='me-2'/>
+                   Store</p>
+                    <p className='fw-bold'>
+                    <img src={Assets.cubeLogo} alt="" className='me-2'/>
+                   Product</p>
+                    <p className='fw-bold'>
+                    <img src={Assets.cartLogo} alt="" className='me-2'/>
+                   Order</p>
+
+                   <Accordion flush>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>
+        <p className='fw-bold'>
+        <img src={Assets.cubeLogo} alt="" className='me-2'/>
+        Product</p>
+        </Accordion.Header>
+        <Accordion.Body>
+            <p className='fw-bold'>My Product</p>
+            <p className='fw-bold'>Seiling Product</p>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+                </div>
+
+        </div>
         {/* <ul>
             <li>
         {data ? data.name : "data not found"}
